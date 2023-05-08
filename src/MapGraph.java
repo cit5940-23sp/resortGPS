25372dbf8ff96e0fcfb6995a773d5e7a4ecac447
PHXWYSAN <116579341+PHXWYSAN@users.noreply.github.com>
2023-05-07 20:06:01 -0400

    add_DFS

--- src/MapGraph.java
+++ src/MapGraph.java
@@ -28,11 +28,43 @@ public class MapGraph  {
         return new ArrayList<>();
     }
 
-    public List<Integer> roundTripPlanning(int src) {
+    public List<Integer> roundTripPlanning(int src, int numStops) {
         // dfs
         return new ArrayList<>();
 
     }
+    // DFS to find all attractions that can be visited within the time window
+    private void DFS(int v, int[] visited, int timeLimit, List<Integer> attractions) {
+        visited[v] = 1; // mark current node as visited
+
+        int[] neighbors = this.g.neighbors(v); // get neighbors of current node
+
+        for (int i = 0; i < neighbors.length; i++) {
+            int w = neighbors[i];
+
+            if (visited[w] == 0 && this.g.weight(v, w) <= timeLimit) {
+                // if neighbor has not been visited and can be visited within time window
+                attractions.add(w); // add attraction to list
+                DFS(w, visited, timeLimit - this.g.weight(v, w), attractions); // recursively call DFS on neighbor
+            }
+        }
+    }
+
+    // Recommend attractions that can be visited within the time window
+    public List<Integer> recommendAttractions(int startingAttraction, int timeLimit) {
+        // array to keep track of visited nodes
+        int[] visited = new int[this.g.nodeCount()];
+        // list to store recommended attractions
+        List<Integer> attractions = new ArrayList<>();
+
+        // start DFS at the starting attraction
+        attractions.add(startingAttraction);
+        DFS(startingAttraction, visited, timeLimit, attractions);
+
+        return attractions;
+    }
+
+
 
 
 
