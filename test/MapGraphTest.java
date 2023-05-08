602a951e67901ed1ff2164506851a1ef911e6570
youshen7 <youshen2@illinois.edu>
2023-05-07 20:20:05 -0400

    add mst part

--- test/MapGraphTest.java
+++ test/MapGraphTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MapGraphTest {
@@ -13,7 +16,24 @@ public class MapGraphTest {
     }
 
     @Test
-    public void findAllDestinations() {
+    public void getMSTPath() {
+        GraphL graph = new GraphL();
+        graph.init(4);
+
+        graph.addEdge(0, 1, 10);
+        graph.addEdge(0, 2, 6);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(1, 2, 15);
+        graph.addEdge(1, 3, 11);
+        graph.addEdge(2, 3, 4);
+
+        MapGraph mapGraph = new MapGraph(graph);
+
+        int startingAttraction = 0;
+        List<Integer> expectedMSTPath = Arrays.asList(0, 3, 2, 1);
+        List<Integer> actualMSTPath = mapGraph.getMSTPath(startingAttraction);
+        assertEquals(expectedMSTPath, actualMSTPath);
+
     }
 
     @Test
